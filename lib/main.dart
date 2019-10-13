3b77a08160d3f2d821cfc6add49a57ce5a5d18c7
Souvenirrr <tungtran130@gmail.com>
2019-10-13 10:18:07 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:piano_games/ui/home_page.dart';
+
+void main() => runApp(MaterialApp(
+  title: "Poke App",
+  home: HomePage(),
+  debugShowCheckedModeBanner: false,
+));
+
